251643401cce0084f990456a2dce2695f8d452f4
oHSUANo <chhu0830@gmail.com>
2017-10-28 18:00:57 +0800

    Add mark email with json data & Revise check web loaded interval

--- extension/trend/classify.js
+++ extension/trend/classify.js
@@ -1,16 +1,29 @@
-var list;
+var json = '[{"id":50,"level":1},{"id":5,"level":0},{"id":10,"level":2}]'
+var color = ["#B54434", "#E2943B", "#F7C242"]
 
-setTimeout(function() {
-	document.getElementsByClassName("zA")[2].style.backgroundColor="red";
-	list = document.getElementById(":iu");
-	list.addEventListener("DOMSubtreeModified", change, false);
-}, 5000);
+check();
 
-function change() {
-	setTimeout(function() {
-		list = document.getElementById(":iu");
-		console.log(list);
-		list.addEventListener("DOMSubtreeModified", change, false);
-		document.getElementsByClassName("zA")[2].style.backgroundColor="red";
-	}, 1000);
+var interval;
+function check() {
+    var list;
+    interval = setInterval(function() {
+        if ((list = document.getElementById(":iu")) != null) {
+            clearInterval(interval);
+            list.addEventListener("DOMSubtreeModified", check, false);
+            var lower = list.childNodes[0].childNodes[0].childNodes[0].innerHTML - 1;
+            var upper = list.childNodes[0].childNodes[0].childNodes[2].innerHTML - 1;
+            mark(lower, upper);
+        }
+    }, 500)
+}
+
+function mark(lower, upper) {
+    var data = JSON.parse(json);
+    for (var i = 0; i < data.length; i++) {
+        var id = data[i]["id"];
+        var level = color[data[i]["level"]];
+        if (id >= lower && id <= upper) {
+            document.getElementsByClassName("zA")[id - lower].style.backgroundColor=level;
+        }
+    }
 }
