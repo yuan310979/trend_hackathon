28f09ba982f0dcd203d4fc1f21d1ed7d41ef048b
oHSUANo <chhu0830@gmail.com>
2017-10-29 11:31:35 +0800

    Add authenticate by auth code & Add mark email by the server result

--- extension/trend/classify.js
+++ extension/trend/classify.js
@@ -1,4 +1,4 @@
-var json = '[{"id":50,"level":1},{"id":5,"level":0},{"id":10,"level":2}]'
+// var json = JSON.parse('[{"id":50,"level":1},{"id":5,"level":0},{"id":10,"level":2}]')
 var color = ["#B54434", "#E2943B", "#F7C242"]
 
 check();
@@ -18,12 +18,24 @@ function check() {
 }
 
 function mark(lower, upper) {
-    var data = JSON.parse(json);
-    for (var i = 0; i < data.length; i++) {
-        var id = data[i]["id"];
-        var level = color[data[i]["level"]];
-        if (id >= lower && id <= upper) {
-            document.getElementsByClassName("zA")[id - lower].style.backgroundColor=level;
+    var data;
+    getParam("level", (data) => {
+        data = JSON.parse(data);
+        for (var i = 0; i < data.length; i++) {
+            var id = data[i]["id"];
+            var level = color[data[i]["level"]];
+            if (id >= lower && id <= upper) {
+                document.getElementsByClassName("zA")[id - lower].style.backgroundColor=level;
+            }
         }
-    }
+    });
+}
+
+function getParam(name, callback) {
+    // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+    // for chrome.runtime.lastError to ensure correctness even when the API call
+    // fails.
+    chrome.storage.sync.get(name, (items) => {
+        callback(chrome.runtime.lastError ? null : items[name]);
+    });
 }
